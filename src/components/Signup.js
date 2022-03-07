eb25cde047f27d104960af0a016b7478812ad75e
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-03-07 13:32:56 +0300

    use formik and yup together completed

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,48 +1,72 @@
-import React from 'react';
+import React from "react";
 import { useFormik } from "formik";
 import validationSchema from "./validation";
 
 function Signup() {
-    const {handleSubmit, handleChange, values} = useFormik({
-        initialValues: {
-        
-          email: "",
-          password:"",
-          passwordConfirm:"",
-        },
-        onSubmit: values => {
-          console.log(values);
-        },
-        validationSchema,
-      });
+  const { handleSubmit, handleChange, handleBlur, values, errors, touched } =
+    useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+        passwordConfirm: "",
+      },
+      onSubmit: (values) => {
+        console.log(values);
+      },
+      validationSchema,
+    });
+
   return (
     <div>
-     <h1>Sign Up</h1>    
-          <form onSubmit={handleSubmit}>
-            
-            <label>Email</label>
-            <input name="email" values={values.email} onChange={handleChange} />
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+          name="email"
+          values={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
 
-            <br />          
-            <br />
-            <label>Password</label>
-            <input name="password" value= {values.password} onChange={handleChange} />
-            <br /> 
-            <br />
-            <label>Confirm Password</label>
-            <input name="passwordConfirm" value= {values.passwordConfirm} onChange={handleChange} />
-            
-            <br />           
-            <br />
+        {errors.email && touched.email && (
+          <div className="error">{errors.email}</div>
+        )}
 
-            <button type="submit">Submit</button>
-            <br />
-            <br />
+        <br />
+        <br />
+        <label>Password</label>
+        <input
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.password && touched.password && (
+          <div className="error">{errors.password}</div>
+        )}
+        <br />
+        <br />
+        <label>Confirm Password</label>
+        <input
+          name="passwordConfirm"
+          value={values.passwordConfirm}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+       {errors.passwordConfirm && touched.passwordConfirm && (
+          <div className="error">{errors.passwordConfirm}</div>
+        )}
+        <br />
+        <br />
 
-            {JSON.stringify(values)}
-          </form>
+        <button type="submit">Submit</button>
+        <br />
+        <br />
+
+        {JSON.stringify(values)}
+      </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
--- src/components/validation.js
+++ src/components/validation.js
@@ -2,9 +2,9 @@ import { object, ref, string} from 'yup';
 
 const validations = object({
  
-  email: string().email().required(),
-  password: string().min(6).required(),
-  passwordConfirm: string().oneOf([ref("password")]).required(),
+  email: string().email("Enter a valid email!").required("Must enter!"),
+  password: string().min(6, "Password must be at least 6 characters").required("Must enter!"),
+  passwordConfirm: string().oneOf([ref("password")], "Passwords must be same!").required("Must enter!"),
   
 });
 
